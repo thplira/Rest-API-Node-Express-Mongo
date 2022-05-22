ec3c16a4499d0314e0fe6307c9fb0a2e0b9f2972
Thales H. P. de Lira <thplira@gmail.com>
2022-05-22 10:32:34 -0300

    FEAT: add an input POST example to populate mongodb

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -3,10 +3,25 @@ import mongoose from 'mongoose';
 const bookSchema = new mongoose.Schema(
     {
         title: {type: String, required: true},
+        subtitle: {type: String, required: false},
         author: {type: String, required: true},
         publisher: {type: String, required: true},
+        isbn: {type: Number, required: true},
         pages: {type: Number}
     }
 );
 
 export default mongoose.model('books', bookSchema);
+
+// HERE AN EXAMPLE IF YOU WANNA TEST AND POPULATE YOUR MONGODB
+// SEND IT IN THE BODY ON A 'POST' RESQUEST
+/**
+{
+    "title": "Clean Code",
+    "subtitle": "A Handbook of Agile Software Craftsmanship",
+    "author": "Robert C. Martin",
+    "publisher": "Pearson",
+    "isbn": "9780136083238",
+    "pages": 431
+}
+*/
